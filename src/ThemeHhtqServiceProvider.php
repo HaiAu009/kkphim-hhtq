197922a1fcf6ebe1a09c65133587e7e5c8f067df
HaiAu009 <nguyenhai.tran.009@gmail.com>
2023-12-22 02:18:47 +0800

    first commit

---
+++ src/ThemeHhtqServiceProvider.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace KKPhim\ThemeHhtq;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class ThemeHhtqServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->setupDefaultThemeCustomizer();
+    }
+
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views/', 'themes');
+
+        $this->publishes([
+            __DIR__ . '/../resources/assets' => public_path('themes/hhtq')
+        ], 'hhtq-assets');
+    }
+
+    protected function setupDefaultThemeCustomizer()
+    {
+        config(['themes' => array_merge(config('themes', []), [
+            'hhtq' => [
+                'name' => 'Theme HHTQ',
+                'author' => 'support@kkphim.com',
+                'package_name' => 'haiau009/kkphim-hhtq',
+                'publishes' => ['hhtq-assets'],
+                'preview_image' => '',
+                'options' => [
+                    [
+                        'name' => 'recommendations_limit',
+                        'label' => 'Recommended movies limit',
+                        'type' => 'number',
+                        'value' => 10,
+                        'wrapperAttributes' => [
+                            'class' => 'form-group col-md-4',
+                        ],
+                        'tab' => 'List'
+                    ],
+                    [
+                        'name' => 'per_page_limit',
+                        'label' => 'Pages limit',
+                        'type' => 'number',
+                        'value' => 18,
+                        'wrapperAttributes' => [
+                            'class' => 'form-group col-md-4',
+                        ],
+                        'tab' => 'List'
+                    ],
+                    [
+                        'name' => 'movie_related_limit',
+                        'label' => 'Movies related limit',
+                        'type' => 'number',
+                        'value' => 10,
+                        'wrapperAttributes' => [
+                            'class' => 'form-group col-md-4',
+                        ],
+                        'tab' => 'List'
+                    ],
+                    [
+                        'name' => 'latest',
+                        'label' => 'Home Page',
+                        'type' => 'code',
+                        'hint' => 'Label|relation|find_by_field|value|sort_by_field|sort_algo|limit|show_more_url',
+                        'value' => "Phim lẻ mới||type|single|updated_at|desc|12|/danh-sach/phim-le\r\nPhim bộ mới||type|series|updated_at|desc|12|/danh-sach/phim-bo\r\nPhim thịnh hành||is_copyright|0|view_week|desc|12|",
+                        'attributes' => [
+                            'rows' => 5
+                        ],
+                        'tab' => 'List'
+                    ],
+                    [
+                        'name' => 'additional_css',
+                        'label' => 'Additional CSS',
+                        'type' => 'code',
+                        'value' => "",
+                        'tab' => 'Custom CSS'
+                    ],
+                    [
+                        'name' => 'body_attributes',
+                        'label' => 'Body attributes',
+                        'type' => 'text',
+                        'value' => 'class="active"',
+                        'tab' => 'Custom CSS'
+                    ],
+                    [
+                        'name' => 'additional_header_js',
+                        'label' => 'Header JS',
+                        'type' => 'code',
+                        'value' => "",
+                        'tab' => 'Custom JS'
+                    ],
+                    [
+                        'name' => 'additional_body_js',
+                        'label' => 'Body JS',
+                        'type' => 'code',
+                        'value' => "",
+                        'tab' => 'Custom JS'
+                    ],
+                    [
+                        'name' => 'additional_footer_js',
+                        'label' => 'Footer JS',
+                        'type' => 'code',
+                        'value' => "",
+                        'tab' => 'Custom JS'
+                    ],
+                    [
+                        'name' => 'footer',
+                        'label' => 'Footer',
+                        'type' => 'code',
+                        'value' => <<<EOT
+                        <div class="myui-foot clearfix">
+                            <div class="container min">
+                                <div class="row">
+                                    <div class="col-pd text-center">
+                                        <p class="margin-0">Liên hệ quảng cáo: admin@gmail.com</p>
+                                        <p class="margin-0">Copyright © 2014-2023 Phim mới | Phim vietsub | Xem phim | Phim Lẻ |
+                                            Phim Online | Phim HD | Download phim</p>
+                                        <p class="margin-0">Disclaimer: This site does not store any files on its server. All contents
+                                            are provided by non-affiliated third parties.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        EOT,
+                        'tab' => 'Custom HTML'
+                    ],
+                    [
+                        'name' => 'ads_header',
+                        'label' => 'Ads header',
+                        'type' => 'code',
+                        'value' => <<<EOT
+                        <img src="" alt="">
+                        EOT,
+                        'tab' => 'Ads'
+                    ],
+                    [
+                        'name' => 'ads_catfish',
+                        'label' => 'Ads catfish',
+                        'type' => 'code',
+                        'value' => <<<EOT
+                        <img src="" alt="">
+                        EOT,
+                        'tab' => 'Ads'
+                    ]
+                ],
+            ]
+        ])]);
+    }
+}
